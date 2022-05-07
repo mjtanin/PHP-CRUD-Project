0a4734402b474647134fe4eebc3f74ef08182657
Tanin Ahmed <taninpabna@gmail.com>
2022-05-07 18:36:54 +0600

    a simple project

---
+++ crud/inc/function.php
@@ -0,0 +1,138 @@
+<?php
+define('DB_NAME', getcwd() . "\\data\\db.json");
+$error = false;
+$message = '';
+
+function seed()
+{
+    $data = array(
+        array(
+            "id" => 1,
+            "fname" => "Tanin",
+            "lname" => "Ahmed",
+            "roll" => 1
+        ),
+        array(
+            "id" => 2,
+            "fname" => "Jui",
+            "lname" => "Akond",
+            "roll" => 2
+        ),
+        array(
+            "id" => 3,
+            "fname" => "Tanvir",
+            "lname" => "Ahmed",
+            "roll" => 3
+        ),
+        array(
+            "id" => 4,
+            "fname" => "Ranu",
+            "lname" => "Akond",
+            "roll" => 4
+        )
+    );
+
+    $json_data = json_encode($data);
+    file_put_contents(DB_NAME, $json_data);
+}
+
+function generateReport()
+{
+    $json_data = file_get_contents(DB_NAME);
+    $students = json_decode($json_data, true);
+
+    if ($students) : ?>
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Roll</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                foreach ($students as $student) { ?>
+                    <tr>
+                        <td><?php printf('%s %s', $student['fname'], $student['lname']); ?></td>
+                        <td><?php printf('%s', $student['roll']) ?></td>
+                        <td style="width: 25%;"><?php printf('<a href="/crud/index.php?task=edit&id=%1$s">Edit</a> | <a href="/crud/index.php?task=delete&id=%1$s">Delete</a>', $student['id']); ?></td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    <?php
+    else : ?>
+        <p>No result found</p>
+<?php endif;
+}
+
+function addstudent($fname, $lname, $roll)
+{
+    $json_data = file_get_contents(DB_NAME);
+    $allstudents = json_decode($json_data, true);
+
+    $ids = [];
+
+    foreach ($allstudents as $student) {
+        array_push($ids, $student['id']);
+        if ($roll == $student['roll']) {
+            return false;
+        }
+    }
+
+    $newstudent = array(
+        'id' => max($ids)  + 1,
+        'fname' => $fname,
+        'lname' => $lname,
+        'roll'  => $roll
+    );
+    array_push($allstudents, $newstudent);
+    $json_data = json_encode($allstudents);
+    file_put_contents(DB_NAME, $json_data);
+    return true;
+}
+
+function deleteStudent($id)
+{
+    $json_data = file_get_contents(DB_NAME);
+    $allstudents = json_decode($json_data, true);
+
+    foreach ($allstudents as $key => $student) {
+        if ($id == $student['id']) {
+            unset($allstudents[$key]);
+        }
+    }
+
+    $json_data = json_encode($allstudents);
+    file_put_contents(DB_NAME, $json_data);
+    header('location: \\crud\\index.php');
+}
+
+
+
+function editStudent($id, $fname, $lname, $roll)
+{
+    $json_data = file_get_contents(DB_NAME);
+    $allstudents = json_decode($json_data, true);
+    $updateStudentList = [];
+
+
+
+    foreach ($allstudents as $student) {
+        if ($roll == $student['roll'] && $id != $student['id']) {
+            return false;
+        }
+
+        if ($id == $student['id']) {
+            $student['fname'] = $fname;
+            $student['lname'] = $lname;
+            $student['roll'] = $roll;
+        }
+        array_push($updateStudentList, $student);
+    }
+
+    $json_data = json_encode($updateStudentList);
+    file_put_contents(DB_NAME, $json_data);
+    return true;
+}
---
+++ crud/index.php
@@ -0,0 +1,125 @@
+<?php
+require_once('./inc/function.php');
+$task = $_GET['task'] ?? 'report';
+$error = $_GET['error'] ?? 0;
+
+$fname = '';
+$lname = '';
+$roll = '';
+$id = '';
+
+if (isset($_POST['submit'])) {
+    $fname = filter_input(INPUT_POST, 'fname', FILTER_SANITIZE_STRING);
+    $lname = filter_input(INPUT_POST, 'lname', FILTER_SANITIZE_STRING);
+    $roll  = filter_input(INPUT_POST, 'roll', FILTER_SANITIZE_STRING);
+    $id  = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_STRING);
+
+    if ($fname == '' || $lname == '' || $roll == '') {
+        header('location: \\crud\\index.php?task=add&error=2');
+        return;
+    }
+
+    if ($id) {
+        $result = editStudent($id, $fname, $lname, $roll);
+        if ($result) {
+            header('location: \\crud\\index.php?task=report');
+        } else {
+            $error = 1;
+        }
+    } else {
+        $result = addstudent($fname, $lname, $roll);
+        if ($result) {
+            header('location: \\crud\\index.php?task=report');
+        } else {
+            $error = 1;
+        }
+    }
+}
+if ('delete' == $task) {
+    deleteStudent($_GET['id']);
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css">
+    <title>PHP CRUD Project</title>
+    <style>
+        body {
+            margin-top: 30px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="column column-60 column-offset-20">
+                <h2>Project - CRUD</h2>
+                <p>A simple prijece to perform CRUD opretions useing plan text file and PHP</p>
+                <?php include_once('./inc/templates/nav.php');
+
+                if ($error == 1) : ?>
+                    <blockquote>
+                        <p>Duplicate Roll</p>
+                    </blockquote>
+                <?php elseif ($error == 2) : ?>
+                    <blockquote>
+                        <p>Please include all fields</p>
+                    </blockquote>
+                <?php endif ?>
+
+
+                <?php if ('seed' === $task) :
+                    seed(); ?>
+                    <p>Seeding Complited</p>
+                <?php endif; ?>
+
+                <?php if ('report' === $task) {
+                    generateReport();
+                } ?>
+                <?php if ('add' === $task) : ?>
+                    <form action="/crud/index.php?task=add" method="POST">
+                        <label for="fname">Fast Name</label>
+                        <input type="text" placeholder="Enter student fast name" id="fname" value="<?php echo $fname; ?>" name="fname">
+                        <label for="lname">Last Name</label>
+                        <input type="text" placeholder="Enter student last name" id="lname" value="<?php echo $lname; ?>" name="lname">
+                        <label for="roll">Roll</label>
+                        <input type="number" placeholder="Enter student roll" id="roll" value="<?php echo $roll; ?>" name="roll">
+                        <button type="submit" class="button" name="submit">Submit</button>
+                    </form>
+                <?php endif; ?>
+                <?php if ('edit' === $task) :
+
+                    $json_data = file_get_contents(DB_NAME);
+                    $allstudents = json_decode($json_data, true);
+
+                    $id = $_GET['id'];
+                    foreach ($allstudents as $student) :
+                        if ($id == $student['id']) : ?>
+                            <form method="POST">
+                                <input type="hidden" name="id" id="id" value="<?php echo $id; ?>">
+                                <label for="fname">Fast Name</label>
+                                <input type="text" placeholder="Enter student fast name" value="<?php echo $student['fname'] ?>" id="fname" name="fname">
+                                <label for="lname">Last Name</label>
+                                <input type="text" placeholder="Enter student last name" value="<?php echo $student['lname'] ?>" id="lname" name="lname">
+                                <label for="roll">Roll</label>
+                                <input type="number" placeholder="Enter student roll" value="<?php echo $student['roll'] ?>" id="roll" name="roll">
+                                <button type="submit" class="button-primary" name="submit">Update</button>
+                            </form>
+                <?php endif;
+                    endforeach;
+                endif; ?>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
